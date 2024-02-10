434915b96a73091e42ae0b29f5b34093ecfbf4b9
Oleksii Kurin <olexkurin@gmail.com>
2024-02-11 00:15:08 +0200

    Add documentation comments and refactoring

--- Backend/Entries.py
+++ Backend/Entries.py
@@ -2,13 +2,42 @@ from datetime import date
 
 
 class DataEntry:
+    """
+    A class for holding data for single news post.
+
+    Attributes:
+    - date (datetime.date): Publishing date of news post.
+    - text (datetime.date): Main text of news post.
+    """
+
     def __init__(self, post_date: date, text: str):
+        """
+        Constructor for DataEntry.
+
+        :param post_date: Publishing date of news post.
+        :param text: Main text of news post.
+        """
+
         self.date = post_date
         self.text = text
 
 
 class AnalysisEntry:
+    """
+    A class for holding NLP analysis results for certain date.
+
+    Attributes:
+    - total_count (int): Total count of news posts.
+    - positive_count (int): Number of posts with positive sentiment.
+    - neutral_count (int): Number of posts with neutral sentiment.
+    - negative_count (int): Number of posts with negative sentiment.
+    """
+
     def __init__(self):
+        """
+        Constructor for DataEntry. Sets all attributes to 0.
+        """
+
         self.total_count = 0
         self.positive_count = 0
         self.neutral_count = 0
--- Backend/main.py
+++ Backend/main.py
@@ -3,8 +3,8 @@ from datetime import date, timedelta, datetime
 from collections import Counter
 from logging import basicConfig, DEBUG
 
-from Backend.ApiKeys import news_api_key, event_registry_api_key
 from Backend.Entries import AnalysisEntry
+from Backend.ApiKeys import news_api_key, event_registry_api_key
 from Backend.DataProviders import NewsApiDataProvider, EventRegistryDataProvider, DataProvider
 
 
@@ -20,21 +20,60 @@ def get_default_analysis(keyword: str, min_post_date: date) -> dict:
     return get_analysis(keyword, min_post_date, default_data_providers, default_max_items_per_provider)
 
 
-def get_analysis(keyword: str,
-                 min_post_date: date,
-                 data_providers: list[DataProvider],
-                 max_items_per_provider: int) -> dict:
+def get_analysis(
+        keyword: str,
+        min_post_date: date,
+        data_providers: list[DataProvider],
+        max_items_per_provider: int) -> dict:
+    """
+    Loads data from data providers, does NLP analysis on it and returns summary results of analysis.
 
+    :param keyword: keyword/phrase to do search on.
+    :param min_post_date: minimum published date for articles.
+    :param data_providers: list of DataProvider object, from which data must be loaded.
+    :param max_items_per_provider: max number of articles to retrieve from every data providers.
+    :return: dictionary in format:
+     {
+        'total':
+        {
+            'count': int,
+            'positive': int,
+            'negative': int,
+            'neutral': itn
+        },
+        'daily':
+        {
+            'dates': [datetime.date],
+            'count': [int],
+            'positive': [int],
+            'negative': [int],
+            'neutral': [int],
+        },
+        'top20_nouns':
+        {
+            'nouns': [str],
+            'count': [int],
+        }
+    }
+    """
+
+    # load DataEntry lists from every provider and concatenate them
     entries = sum([provider.load_data(keyword, min_post_date, max_items_per_provider)
                    for provider in data_providers], [])
 
-    total_entry = AnalysisEntry()
-    dates_entries = {}
-    noun_phrases = []
+    total_entry = AnalysisEntry()  # entry for holding total statistics data
+    dates_entries = {}  # dictionary of AnalysisEntry for every date
+    noun_phrases = []  # list of all nouns, recognized in articles
+
+    # do analysis for every DataEntry
     for data_entry in entries:
+        # receive or create new DataEntry for certain day
         entry = dates_entries.setdefault(data_entry.date, AnalysisEntry())
 
+        # do NLP analysis for article text
         blob = TextBlob(data_entry.text)
+
+        # get text sentiment data
         if blob.sentiment.polarity > 0:
             entry.positive_count += 1
             total_entry.positive_count += 1
@@ -45,14 +84,18 @@ def get_analysis(keyword: str,
             entry.neutral_count += 1
             total_entry.neutral_count += 1
 
+        # increase total counts
         entry.total_count += 1
         total_entry.total_count += 1
 
+        # add recognized nouns to nouns list
         noun_phrases += blob.noun_phrases
 
+    # sort dates_entries by date
     dates_entries = dict(sorted(dates_entries.items()))
     most_common_nouns = Counter(noun_phrases).most_common(20)
 
+    # return summary data of NLP
     return {
         'total': {
             'count': total_entry.total_count,
